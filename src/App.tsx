729c79a1421ea18b5138805b3535728d82a1c2e6
Shilpa Shendre <Shilpashendre@WinjitTechnologiesPvtLtd.local>
2020-05-19 18:14:23 +0530

    feat: Add Semantic Release and Github Actions6

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// minor chnage to see version reflection
+
 function App() {
   return (
     <div className="App">
